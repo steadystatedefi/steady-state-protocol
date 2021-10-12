c0067aac20b556877c4a38cb2604442d4b9b2527
Sleepingshell <Sleepingshell@github.com>
2021-10-12 09:56:16 -0500

    The Virtual Reserve aka Vault holds the USDX tokens for a pool. It will contain the rules on when money can be transferred out (i.e a claim filed)

---
+++ contracts/interfaces/IVirtualReserve.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+/// @dev Holds the USDX tokens for a particular protocol. This vault will contain the rules about
+///     the underlying USDX backing a protocol, NOT the rules regarding the ERC20
+interface IVirtualReserve {
+  /// @dev deposit will be called by the owning pool, transferring the USDX tokens. Does not keep track of owners
+  function deposit(address owner, uint256 amount) external;
+}
