a3de8fdf2fd3e8b91d601627abc1e750be48b6f2
Cyrax <90716326+cyrax-steady@users.noreply.github.com>
2021-10-14 22:11:40 +0300

    Stub for config

--- helpers/config_loader.ts
+++ helpers/config_loader.ts
@@ -9,9 +9,10 @@ export const loadTestConfig = () => loadRuntimeConfig(ConfigNames.Test);
 
 export const loadRuntimeConfig = (configName: ConfigNames): IRuntimeConfig => {
   switch (configName) {
-    // case ConfigNames.Test:
+    case ConfigNames.Test:
+      return undefined;
     //   return TestConfig;
     default:
-      throw new Error(`Unsupported pool configuration: ${Object.values(ConfigNames)}`);
+      throw new Error(`Unsupported pool configuration: ${configName} ${Object.values(ConfigNames)}`);
   }
 };
