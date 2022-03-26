4edc685fae36317c7e87b1f9f3450ff2b332e658
SleepingShell <tyler@steadystate.finance>
2022-03-25 22:46:18 -0500

    Copy mock stable from collateral-fund

---
+++ contracts/collateralFund/mocks/MockStablecoin.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
+contract MockStable is ERC20 {
+  address private owner;
+
+  constructor() ERC20('Stable', 'STBL') {
+    owner = msg.sender;
+  }
+
+  ///@dev Currently anyone can mint
+  function mint(address to, uint256 amount) external returns (bool) {
+    //require(msg.sender == owner);
+
+    _mint(to, amount);
+    return true;
+  }
+
+  ///@dev Currently anyone can burn
+  function burn(address to, uint256 amount) external returns (bool) {
+    //require(msg.sender == owner);
+
+    _burn(to, amount);
+    return true;
+  }
+}
