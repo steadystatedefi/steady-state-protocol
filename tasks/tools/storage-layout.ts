b3ea3341e541d7d14728981149137931d830d54f
cyrax-steady <90716326+cyrax-steady@users.noreply.github.com>
2022-06-08 22:57:04 +0300

    Storage layout command (#25)

---
+++ tasks/tools/storage-layout.ts
@@ -0,0 +1,8 @@
+import { task } from 'hardhat/config';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+
+task('storage-layout', 'Print storage layout of contracts').setAction(async (_, DRE: HardhatRuntimeEnvironment) => {
+  await DRE.run('set-DRE');
+
+  await DRE.storageLayout.export();
+});
