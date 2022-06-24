64a2b7dd3ca6bf09edd3bed66fda0625eb26b6f5
Cyrax <90716326+cyrax-steady@users.noreply.github.com>
2022-06-24 20:59:49 +0400

    Initial

---
+++ contracts/funds/CollateralFundBase.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity ^0.8.4;
+
+import '../tools/SafeOwnable.sol';
+
+contract CollateralFundBase {
+  // function mint(address account, uint256 amount) external onlyWithFlags(FLAG_MINT) {
+  //   _mint(account, amount);
+  // }
+  // function mintAndTransfer(
+  //   address onBehalf,
+  //   address recepient,
+  //   uint256 amount
+  // ) external onlyWithFlags(FLAG_MINT) {
+  //   _mintAndTransfer(onBehalf, recepient, amount);
+  // }
+  // function burn(address account, uint256 amount) external onlyWithFlags(FLAG_BURN) {
+  //   _burn(account, amount);
+  // }
+}
