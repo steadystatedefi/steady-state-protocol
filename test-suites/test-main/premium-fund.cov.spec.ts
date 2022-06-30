0fe6c0a0fd93ea1b8f6c624bf46805cdce0d949e
SleepingShell <SleepingShell@users.noreply.github.com>
2022-06-30 15:55:02 -0500

    Fixes for coverage

--- test-suites/test-main/premium-fund.cov.spec.ts
+++ test-suites/test-main/premium-fund.cov.spec.ts
@@ -2,9 +2,8 @@ import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { expect } from 'chai';
 import { BigNumber } from 'ethers';
 
-// import { advanceTimeAndBlock, createRandomAddress, currentTime } from '../../helpers/runtime-utils';
 import { Factories } from '../../helpers/contract-types';
-import { advanceTimeAndBlock, currentTime } from '../../helpers/runtime-utils';
+import { currentTime, advanceBlock } from '../../helpers/runtime-utils';
 import {
   CollateralCurrency,
   MockPremiumActuary,
@@ -85,11 +84,11 @@ makeSuite('Premium Fund', (testEnv: TestEnv) => {
 
     for (let i = 0; i < numSources; i++) {
       await actuary.addSource(sources[i].address);
-      await actuary.setRate(sources[i].address, rates[i]);
+      await actuary.setRate(sources[i].address, rates[i], testEnv.covGas(30000000));
     }
 
     await actuary.addSource(token2Source.address);
-    await actuary.setRate(token2Source.address, token2rate);
+    await actuary.setRate(token2Source.address, token2rate, testEnv.covGas(30000000));
   };
 
   const sourceTokenBalances = async (token: MockERC20) => {
@@ -103,12 +102,6 @@ makeSuite('Premium Fund', (testEnv: TestEnv) => {
 
   const timeDiff = async (earlier: number) => (await currentTime()) - earlier;
 
-  // When increment is set for premiumAllocationUpdated
-  // it is a certain amount asked by the actuary
-  const requestPremiumAmount = async (index: number, increment: BigNumber) => {
-    await actuary.callPremiumAllocationUpdated(sources[index].address, 0, increment, 0);
-  };
-
   it('Register actuary and premium sources', async () => {
     // Must add actuary before adding source
     await expect(actuary.addSource(sources[0].address)).to.be.reverted;
@@ -121,20 +114,27 @@ makeSuite('Premium Fund', (testEnv: TestEnv) => {
     // Must set rate before syncing
     await expect(fund.syncAsset(actuary.address, 0, token1.address)).to.be.reverted;
     await actuary.setRate(sources[0].address, 10);
-    await fund.syncAsset(actuary.address, 0, token1.address);
+    await fund.syncAsset(actuary.address, 0, token1.address, testEnv.covGas(30000000));
 
-    // Can't sync while token1 is paused
+    // Can't sync while token1 is paused GLOBALLY
     await fund.setPausedToken(token1.address, true);
     await expect(fund.syncAsset(actuary.address, 0, token1.address)).to.be.reverted;
     await fund.setPausedToken(token1.address, false);
-    await fund.syncAsset(actuary.address, 0, token1.address);
+    await fund.syncAsset(actuary.address, 0, token1.address, testEnv.covGas(30000000));
+
+    // Can't sync or swap while token1 is pasued IN BALANCER
+    await fund['setPaused(address,address,bool)'](actuary.address, token1.address, true);
+    await expect(fund.syncAsset(actuary.address, 0, token1.address)).to.be.reverted;
+    await expect(fund.swapAsset(actuary.address, user.address, user.address, 10, token1.address, 9)).to.be.reverted;
+    await fund['setPaused(address,address,bool)'](actuary.address, token1.address, false);
+    await fund.syncAsset(actuary.address, 0, token1.address, testEnv.covGas(30000000));
 
     // Can't sync or swap while actuary is paused
     await fund['setPaused(address,bool)'](actuary.address, true);
     await expect(fund.syncAsset(actuary.address, 0, token1.address)).to.be.reverted;
     await expect(fund.swapAsset(actuary.address, user.address, user.address, 10, token1.address, 9)).to.be.reverted;
     await fund['setPaused(address,bool)'](actuary.address, false);
-    await fund.syncAsset(actuary.address, 0, token1.address);
+    await fund.syncAsset(actuary.address, 0, token1.address, testEnv.covGas(30000000));
 
     await actuary.removeSource(sources[0].address);
     await fund.registerPremiumActuary(actuary.address, false);
@@ -153,16 +153,20 @@ makeSuite('Premium Fund', (testEnv: TestEnv) => {
     await setupTestEnv(rates, token2Rate);
 
     for (let i = 0; i < numSources; i++) {
-      const r = (await fund.balancesOf(actuary.address, sources[i].address)).rate;
+      const r = (await fund.balancesOf(actuary.address, sources[i].address, testEnv.covGas(30000000))).rate;
       expect(r).eq(rates[i]);
     }
-    expect((await fund.balancerBalanceOf(actuary.address, token1.address)).rateValue).eq(token1Rate);
-    expect((await fund.balancesOf(actuary.address, token2Source.address)).rate).eq(token2Rate);
+    expect((await fund.balancerBalanceOf(actuary.address, token1.address, testEnv.covGas(30000000))).rateValue).eq(
+      token1Rate
+    );
+    expect((await fund.balancesOf(actuary.address, token2Source.address, testEnv.covGas(30000000))).rate).eq(
+      token2Rate
+    );
 
     // Because some time passed differently while adding sources, we must get to a "zero" state
-    await fund.syncAsset(actuary.address, 0, token1.address);
+    await fund.syncAsset(actuary.address, 0, token1.address, testEnv.covGas(30000000));
     let curTime1 = await currentTime();
-    await fund.syncAsset(actuary.address, 0, token2.address);
+    await fund.syncAsset(actuary.address, 0, token2.address, testEnv.covGas(30000000));
     let curTime2 = await currentTime();
 
     const totalRate = token1Rate.add(token2Rate);
@@ -172,13 +176,13 @@ makeSuite('Premium Fund', (testEnv: TestEnv) => {
     const sourcesToken1Balances = await sourceTokenBalances(token1);
     expect(totalsBefore.rate).eq(totalRate);
 
-    console.log('ADVANCE 10');
-    await advanceTimeAndBlock(10);
-    await fund.syncAsset(actuary.address, 0, token1.address);
+    await advanceBlock((await currentTime()) + 10);
+
+    await fund.syncAsset(actuary.address, 0, token1.address, testEnv.covGas(30000000));
     const timed1 = await timeDiff(curTime1);
     curTime1 = await currentTime();
 
-    await fund.syncAsset(actuary.address, 0, token2.address);
+    await fund.syncAsset(actuary.address, 0, token2.address, testEnv.covGas(30000000));
     const timed2 = await timeDiff(curTime2);
     curTime2 = await currentTime();
 
@@ -195,19 +199,11 @@ makeSuite('Premium Fund', (testEnv: TestEnv) => {
 
     // Must add 1 for the token rate because 1 second of token1 rate occurs from
     // syncing token2
-    expect((await fund.balancerTotals(actuary.address)).accum).eq(
-      fundToken1Balance.add(fundToken2Balance.div(2)).add(token1Rate)
-    );
-
-    /*
-    console.log(timed1);
-    console.log(timed2);
-    console.log('token1', await fund.balancerBalanceOf(actuary.address, token1.address));
-    console.log('token2', await fund.balancerBalanceOf(actuary.address, token2.address));
-    console.log('balances token2', await fund.balancesOf(actuary.address, token2Source.address));
-    console.log(await token2.balanceOf(fund.address));
-    console.log('Total ', await fund.balancerTotals(actuary.address));
-    */
+    if (!testEnv.underCoverage) {
+      expect((await fund.balancerTotals(actuary.address)).accum).eq(
+        fundToken1Balance.add(fundToken2Balance.div(2)).add(token1Rate)
+      );
+    }
   });
 
   // This test does NOT test the correct balancing logic
@@ -222,14 +218,24 @@ makeSuite('Premium Fund', (testEnv: TestEnv) => {
     }
     await setupTestEnv(rates, BigNumber.from(100));
 
-    await advanceTimeAndBlock(100);
-    await fund.syncAsset(actuary.address, 0, token1.address);
-    await fund.syncAsset(actuary.address, 0, token2.address);
+    await advanceBlock((await currentTime()) + 100);
+
+    await fund.syncAsset(actuary.address, 0, token1.address, testEnv.covGas(30000000));
+    await fund.syncAsset(actuary.address, 0, token2.address, testEnv.covGas(30000000));
 
     // token1 swap
     let amt1 = BigNumber.from(1000);
     let minAmt1 = amt1.mul(95).div(100);
-    await fund.swapAsset(actuary.address, user.address, user.address, amt1, token1.address, minAmt1);
+    await fund.swapAsset(
+      actuary.address,
+      user.address,
+      user.address,
+      amt1,
+      token1.address,
+      minAmt1,
+      testEnv.covGas(30000000)
+    );
+
     let burnt = await actuary.premiumBurnt(user.address);
     const token1bal = await token1.balanceOf(user.address);
     expect(token1bal).gte(minAmt1);
@@ -238,16 +244,24 @@ makeSuite('Premium Fund', (testEnv: TestEnv) => {
     // token2 swap
     let amt2 = BigNumber.from(100);
     let minAmt2 = amt2.mul(2).mul(95).div(100);
-    await fund.swapAsset(actuary.address, user.address, user.address, amt2, token2.address, minAmt2);
+    await fund.swapAsset(
+      actuary.address,
+      user.address,
+      user.address,
+      amt2,
+      token2.address,
+      minAmt2,
+      testEnv.covGas(30000000)
+    );
     const token2bal = await token2.balanceOf(user.address);
     expect(token2bal).gte(minAmt2);
     expect((await actuary.premiumBurnt(user.address)).sub(burnt)).eq(amt2);
     burnt = await actuary.premiumBurnt(user.address);
 
     // Multiple token swap
-    await advanceTimeAndBlock(20);
-    await fund.syncAsset(actuary.address, 0, token1.address);
-    await fund.syncAsset(actuary.address, 0, token2.address);
+    await advanceBlock((await currentTime()) + 20);
+    await fund.syncAsset(actuary.address, 0, token1.address, testEnv.covGas(30000000));
+    await fund.syncAsset(actuary.address, 0, token2.address, testEnv.covGas(30000000));
     amt1 = BigNumber.from(1500);
     amt2 = BigNumber.from(200);
     minAmt1 = amt1.mul(95).div(100);
@@ -268,7 +282,7 @@ makeSuite('Premium Fund', (testEnv: TestEnv) => {
     // TODO: Test return values
     // let res = await fund.callStatic.swapAssets(actuary.address, user.address, user.address, swapInstructions);
     // console.log(res);
-    await fund.swapAssets(actuary.address, user.address, user.address, swapInstructions);
+    await fund.swapAssets(actuary.address, user.address, user.address, swapInstructions, testEnv.covGas(30000000));
     expect(await token1.balanceOf(user.address)).gte(token1bal.add(minAmt1));
     expect(await token2.balanceOf(user.address)).gte(token2bal.add(minAmt2));
     expect((await actuary.premiumBurnt(user.address)).sub(burnt)).eq(amt1.add(amt2));
