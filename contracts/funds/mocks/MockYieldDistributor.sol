563e3f006bce2a7ca9ffb9a72c01567ff246a2ed
Cyrax <90716326+cyrax-steady@users.noreply.github.com>
2022-08-01 11:48:01 +0400

    Mock

---
+++ contracts/funds/mocks/MockYieldDistributor.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity ^0.8.4;
+
+import '../YieldDistributorBase.sol';
+
+contract MockYieldDistributor is YieldDistributorBase {
+  mapping(address => uint256) private _prices;
+
+  constructor(address collateral_) YieldDistributorBase(IAccessController(address(0)), collateral_) {}
+
+  function hasAnyAcl(address, uint256) internal pure override returns (bool) {
+    return true;
+  }
+
+  function hasAllAcl(address, uint256) internal pure override returns (bool) {
+    return true;
+  }
+}
