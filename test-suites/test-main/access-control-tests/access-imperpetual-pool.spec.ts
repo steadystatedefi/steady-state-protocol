5d035c1bb5d1eca52e1dc62c615a8523eda14be1
SleepingShell <SleepingShell@users.noreply.github.com>
2022-08-11 14:19:43 -0500

    Insurer governor

--- test-suites/test-main/access-control-tests/access-imperpetual-pool.spec.ts
+++ test-suites/test-main/access-control-tests/access-imperpetual-pool.spec.ts
@@ -1,7 +1,7 @@
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { expect } from 'chai';
 
-import { INSURER_ADMIN, INSURER_OPS, LP_DEPLOY } from '../../../helpers/access-control-constants';
+import { AccessFlags } from '../../../helpers/access-flags';
 import { WeightedPoolParamsStruct } from '../../../types/contracts/insurer/ImperpetualPoolBase';
 import { makeSuite, TestEnv } from '../setup/make-suite';
 
@@ -14,6 +14,20 @@ makeSuite('access: Imperpetual Pool', (testEnv: TestEnv) => {
   let user2: SignerWithAddress;
   let user3: SignerWithAddress;
 
+  const params: WeightedPoolParamsStruct = {
+    maxAdvanceUnits: 100_000_000,
+    minAdvanceUnits: 1_000,
+    riskWeightTarget: 10_00,
+    minInsuredSharePct: 1_00,
+    maxInsuredSharePct: 15_00,
+    minUnitsPerRound: 10,
+    maxUnitsPerRound: 20,
+    overUnitsPerRound: 30,
+    coveragePrepayPct: 100_00,
+    maxUserDrawdownPct: 0,
+    unitsPerAutoPull: 0,
+  };
+
   before(async () => {
     deployer = testEnv.deployer;
     user2 = testEnv.users[2];
@@ -22,8 +36,18 @@ makeSuite('access: Imperpetual Pool', (testEnv: TestEnv) => {
 
     await setInsurer(state, deployer, user2.address);
 
-    await state.controller.grantRoles(deployer.address, INSURER_ADMIN.or(LP_DEPLOY));
+    await state.controller.grantRoles(deployer.address, AccessFlags.INSURER_ADMIN | AccessFlags.LP_DEPLOY);
     await state.cc.registerInsurer(state.insurer.address);
+    await state.controller.revokeRoles(deployer.address, AccessFlags.INSURER_ADMIN);
+  });
+
+  it('ROLE: Governor', async () => {
+    await state.insured.joinPool(state.insurer.address);
+    await expect(state.insurer.approveJoiner(state.insured.address, true)).to.be.reverted;
+    await expect(state.insurer.setPoolParams(params)).to.be.reverted;
+
+    await state.insurer.connect(user2).approveJoiner(state.insured.address, true);
+    await state.insurer.connect(user2).setPoolParams(params);
   });
 
   it('ROLE: Insurer Ops', async () => {
@@ -31,38 +55,26 @@ makeSuite('access: Imperpetual Pool', (testEnv: TestEnv) => {
     await expect(state.insurer.approveJoiner(state.insured.address, true)).to.be.reverted;
     await expect(state.insurer.addSubrogation(user2.address, 0)).to.be.reverted;
 
-    await state.controller.grantRoles(deployer.address, INSURER_OPS);
+    await state.controller.grantRoles(deployer.address, AccessFlags.INSURER_OPS);
     await state.insurer.approveJoiner(state.insured.address, true);
     await state.insurer.addSubrogation(user2.address, 0);
+
+    // TODO: onlyActiveInsuredOrOps
   });
 
   it('ROLE: Insurer Admin', async () => {
-    const params: WeightedPoolParamsStruct = {
-      maxAdvanceUnits: 100_000_000,
-      minAdvanceUnits: 1_000,
-      riskWeightTarget: 10_00,
-      minInsuredSharePct: 1_00,
-      maxInsuredSharePct: 15_00,
-      minUnitsPerRound: 10,
-      maxUnitsPerRound: 20,
-      overUnitsPerRound: 30,
-      coveragePrepayPct: 100_00,
-      maxUserDrawdownPct: 0,
-      unitsPerAutoPull: 0,
-    };
-
     await expect(state.insurer.connect(user3).setGovernor(user3.address)).to.be.reverted;
     await expect(state.insurer.connect(user3).setPremiumDistributor(user3.address)).to.be.reverted;
     await expect(state.insurer.connect(user3).setPoolParams(params)).to.be.reverted;
 
-    await state.controller.grantRoles(deployer.address, INSURER_ADMIN);
+    await state.controller.grantRoles(deployer.address, AccessFlags.INSURER_ADMIN);
     await state.insurer.setGovernor(user3.address);
     await state.insurer.setPremiumDistributor(user3.address);
     await state.insurer.setPoolParams(params);
   });
 
   it('ROLE: Collateral Currency', async () => {
-    await state.controller.grantRoles(deployer.address, INSURER_ADMIN);
+    await state.controller.grantRoles(deployer.address, AccessFlags.INSURER_ADMIN);
     await state.cc.registerLiquidityProvider(deployer.address);
 
     await expect(state.insurer.onTransferReceived(user2.address, user2.address, 100, '')).to.be.reverted;
@@ -70,4 +82,6 @@ makeSuite('access: Imperpetual Pool', (testEnv: TestEnv) => {
   });
 
   // TODO: ImperpetualPoolBase onlySelf
+
+  // TODO: WeightedPoolBase OnlyPremiumDistributor
 });
