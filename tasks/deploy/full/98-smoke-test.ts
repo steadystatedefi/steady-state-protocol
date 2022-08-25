a2f5aac92c1e37453188db91ef83c5b6000d25f5
cyrax-steady <90716326+cyrax-steady@users.noreply.github.com>
2022-08-25 16:07:56 +0400

    Improve access checker for goerli (#68)

--- tasks/deploy/full/98-smoke-test.ts
+++ tasks/deploy/full/98-smoke-test.ts
@@ -1,11 +1,14 @@
 import { task } from 'hardhat/config';
 
 import { ConfigNamesAsString } from '../../../helpers/config-loader';
+import { dreAction } from '../../../helpers/dre';
 
 task(`full:smoke-test`, 'Smoke test')
-  .addParam('cfg', `Configuration name: ${ConfigNamesAsString}`)
-  .setAction(async () => {
-    // await localBRE.run('set-DRE');
-    // const network = <eNetwork>localBRE.network.name;
-    // const poolConfig = loadRuntimeConfig(pool);
-  });
+  .addOptionalParam('cfg', `Configuration name: ${ConfigNamesAsString}`)
+  .setAction(
+    dreAction(async () => {
+      // await localBRE.run('set-DRE');
+      // const network = <eNetwork>localBRE.network.name;
+      // const poolConfig = loadRuntimeConfig(pool);
+    })
+  );
