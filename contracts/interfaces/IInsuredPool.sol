ee31ff8493b6210d6b39d13671c436c54f046ef1
Cyrax <90716326+cyrax-steady@users.noreply.github.com>
2022-10-21 15:39:13 +0400

    Code doc on some interfaces

--- contracts/interfaces/IInsuredPool.sol
+++ contracts/interfaces/IInsuredPool.sol
@@ -28,29 +28,46 @@ interface IInsuredPool is ICollateralized {
   /// @return rate The rate that the insured is paying for the coverage
   function offerCoverage(uint256 offeredAmount) external returns (uint256 acceptedAmount, uint256 rate);
 
+  /// @dev Information about configured rate bands.
+  /// @return bands with information of rate bands. Length is <= maxBands.
+  /// @return maxBands with a maximum number of rate bands supported by this implementation of insured.
   function rateBands() external view returns (InsuredRateBand[] memory bands, uint256 maxBands);
 
-  function getInsurers() external view returns (address[] memory, address[] memory);
+  /// @dev Returns insurers joined by this insured
+  /// @return nonChartered list with insurers without demand chartering. See ICharterable.
+  /// @return chartered list with insurers with demand chartering. See ICharterable.
+  function getInsurers() external view returns (address[] memory nonChartered, address[] memory chartered);
+}
+
+struct InsuredParams {
+  /// @dev A minimum amount of coverage demand to be accepted by an insurer
+  uint128 minPerInsurer;
+}
+
+/// @dev Information about the rate band
+struct InsuredRateBand {
+  /// @dev Premium rate for this band
+  uint256 premiumRate;
+  /// @dev Amount of coverage demand that can be given to insurer(s) under this premiumRate
+  uint256 coverageDemand;
+  /// @dev Amount of coverage demand already given to insurer(s) under this premiumRate
+  uint256 assignedDemand;
 }
 
 interface IReconcilableInsuredPool is IInsuredPool {
+  /// @return information about coverage that can be given to this insured after reconciliation with the `insurer`
   function receivableByReconcileWithInsurer(address insurer) external view returns (ReceivableByReconcile memory);
 }
 
 struct ReceivableByReconcile {
+  /// @dev amount of coverage to be added to escrow (<= providedCoverage)
   uint256 receivableCoverage;
+  /// @dev amount of coverage demanded
   uint256 demandedCoverage;
+  /// @dev amount of demanded covered (<= demandedCoverage)
   uint256 providedCoverage;
+  /// @dev current premium rate (for providedCoverage, considering rate bands)
   uint256 rate;
+  /// @dev time-cumulated premium rate
   uint256 accumulated;
-}
-
-struct InsuredParams {
-  uint128 minPerInsurer;
-}
-
-struct InsuredRateBand {
-  uint256 premiumRate;
-  uint256 coverageDemand;
-  uint256 assignedDemand;
 }
